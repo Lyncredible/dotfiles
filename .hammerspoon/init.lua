b1a8dc4743baa272f34dc4a07fc111a279c89ed4
Yuan Liu <lyncredible@outlook.com>
2018-07-02 14:34:29 -0700

    More window management

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,101 +1,103 @@
 hs.application.enableSpotlightForNameSearches(true)
 hs.window.animationDuration = 0
 
-local baseKey = {"cmd", "ctrl"}
-local hyperKey = {"cmd", "alt", "ctrl"}
+local BaseKey = {"cmd", "ctrl"}
+local HyperKey = {"cmd", "alt", "ctrl"}
+local UniversalKey = {"cmd", "alt", "ctrl", "shift"}
+local Direction = { Left = "Left", Up = "Up", right = "Right", down = "Down" }
 
-hs.hotkey.bind(baseKey, "S", function()
+hs.hotkey.bind(BaseKey, "S", function()
   hs.spotify.playpause()
 end)
 
-hs.hotkey.bind(baseKey, "D", function()
+hs.hotkey.bind(BaseKey, "D", function()
   hs.spotify.displayCurrentTrack()
 end)
 
-hs.hotkey.bind(baseKey, "B", function()
+hs.hotkey.bind(BaseKey, "B", function()
   hs.spotify.previous()
 end)
 
-hs.hotkey.bind(baseKey, "F", function()
+hs.hotkey.bind(BaseKey, "F", function()
   hs.spotify.next()
 end)
 
-hs.hotkey.bind(baseKey, "M", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
+hs.hotkey.bind(BaseKey, "M", function()
+  local window = hs.window.focusedWindow()
+  local f = window:frame()
+  local screen = window:screen()
   local max = screen:frame()
 
   f.x = max.x
   f.y = max.y
   f.w = max.w
   f.h = max.h
-  win:setFrame(f)
+  window:setFrame(f)
 end)
 
-hs.hotkey.bind(baseKey, "O", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
+hs.hotkey.bind(BaseKey, "O", function()
+  local window = hs.window.focusedWindow()
+  local f = window:frame()
+  local screen = window:screen()
   local max = screen:frame()
 
   f.x = max.x + (max.w / 6)
   f.y = max.y + (max.h / 6)
   f.w = max.w * 2 / 3
   f.h = max.h * 2 / 3
-  win:setFrame(f)
+  window:setFrame(f)
 end)
 
-hs.hotkey.bind(baseKey, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
+hs.hotkey.bind(BaseKey, "Left", function()
+  local window = hs.window.focusedWindow()
+  local f = window:frame()
+  local screen = window:screen()
   local max = screen:frame()
 
   f.x = max.x
   f.y = max.y
   f.w = max.w / 2
   f.h = max.h
-  win:setFrame(f)
+  window:setFrame(f)
 end)
 
-hs.hotkey.bind(baseKey, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
+hs.hotkey.bind(BaseKey, "Right", function()
+  local window = hs.window.focusedWindow()
+  local f = window:frame()
+  local screen = window:screen()
   local max = screen:frame()
 
   f.x = max.x + (max.w / 2)
   f.y = max.y
   f.w = max.w / 2
   f.h = max.h
-  win:setFrame(f)
+  window:setFrame(f)
 end)
 
-hs.hotkey.bind(baseKey, "Up", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
+hs.hotkey.bind(BaseKey, "Up", function()
+  local window = hs.window.focusedWindow()
+  local f = window:frame()
+  local screen = window:screen()
   local max = screen:frame()
 
   f.x = max.x
   f.y = max.y
   f.w = max.w
   f.h = max.h / 2
-  win:setFrame(f)
+  window:setFrame(f)
 end)
 
-hs.hotkey.bind(baseKey, "Down", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
+hs.hotkey.bind(BaseKey, "Down", function()
+  local window = hs.window.focusedWindow()
+  local f = window:frame()
+  local screen = window:screen()
   local max = screen:frame()
 
   f.x = max.x
   f.y = max.y + (max.h / 2)
   f.w = max.w
   f.h = max.h / 2
-  win:setFrame(f)
+  window:setFrame(f)
 end)
 
 function shouldRetainSizeAcrossScreen(window)
@@ -103,24 +105,66 @@ function shouldRetainSizeAcrossScreen(window)
   return app == 'iTerm2' or app == 'Terminal'
 end
 
-hs.hotkey.bind(hyperKey, "Left", function()
-  local win = hs.window.focusedWindow()
-  retainSize = shouldRetainSizeAcrossScreen(win)
+function moveWindowOneScreen(window, direction)
+  retainSize = shouldRetainSizeAcrossScreen(window)
   ensureInScreenBounds = false
-  win:moveOneScreenWest(retainSize, ensureInScreenBounds)
-  if retainSize then
-    win:centerOnScreen()
+
+  if direction == Direction.Left then
+    window:moveOneScreenWest(retainSize, ensureInScreenBounds)
+  elseif direction == Direction.Right then
+    window:moveOneScreenEast(retainSize, ensureInScreenBounds)
+  elseif direction == Direction.Up then
+    window:moveOneScreenNorth(retainSize, ensureInScreenBounds)
+  elseif direction == Direction.Down then
+    window:moveOneScreenSouth(retainSize, ensureInScreenBounds)
   end
+
+  if retainSize then
+    window:centerOnScreen()
+  end
+end
+
+function moveAllWindowsOneScreen(direction)
+  local windows = hs.window.allWindows()
+  hs.fnutils.each(windows, function(window)
+    moveWindowOneScreen(window, direction)
+  end)
+end
+
+hs.hotkey.bind(HyperKey, "Left", function()
+  local window = hs.window.focusedWindow()
+  moveWindowOneScreen(window, Direction.Left)
 end)
 
-hs.hotkey.bind(hyperKey, "Right", function()
-  local win = hs.window.focusedWindow()
-  local retainSize = shouldRetainSizeAcrossScreen(win)
-  local ensureInScreenBounds = false
-  win:moveOneScreenEast(retainSize, ensureInScreenBounds)
-  if retainSize then
-    win:centerOnScreen()
-  end
+hs.hotkey.bind(HyperKey, "Right", function()
+  local window = hs.window.focusedWindow()
+  moveWindowOneScreen(window, Direction.Right)
+end)
+
+hs.hotkey.bind(HyperKey, "Up", function()
+  local window = hs.window.focusedWindow()
+  moveWindowOneScreen(window, Direction.Up)
+end)
+
+hs.hotkey.bind(HyperKey, "Down", function()
+  local window = hs.window.focusedWindow()
+  moveWindowOneScreen(window, Direction.Down)
+end)
+
+hs.hotkey.bind(UniversalKey, "Left", function()
+  moveAllWindowsOneScreen(Direction.Left)
+end)
+
+hs.hotkey.bind(UniversalKey, "Right", function()
+  moveAllWindowsOneScreen(Direction.Right)
+end)
+
+hs.hotkey.bind(UniversalKey, "Up", function()
+  moveAllWindowsOneScreen(Direction.Up)
+end)
+
+hs.hotkey.bind(UniversalKey, "Down", function()
+  moveAllWindowsOneScreen(Direction.Down)
 end)
 
 function centerInParent(size, parentRect)
@@ -143,7 +187,7 @@ function codingLayout()
   hs.layout.apply(windowLayout)
 end
 
-hs.hotkey.bind(baseKey, "P", codingLayout)
+hs.hotkey.bind(BaseKey, "P", codingLayout)
 
 local screenWatcher = hs.screen.watcher.new(codingLayout)
 screenWatcher:start()
@@ -164,7 +208,7 @@ function toggleKSheet()
   end
 end
 
-hs.hotkey.bind(baseKey, "K", toggleKSheet)
+hs.hotkey.bind(BaseKey, "K", toggleKSheet)
 
 spoon.SpoonInstall:andUse("ReloadConfiguration", {fn = function()
   spoon.ReloadConfiguration:start()
